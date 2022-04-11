aa17432ec5db031cce076d31cff949d75238b7d4
Vladimir Vainer <str3l0k@gmail.com>
2022-04-11 11:35:32 +0300

    added eggTimer module

---
+++ eggtimer/src/main/java/com/vvainer/eggtimer/EggTimer.kt
@@ -0,0 +1,43 @@
+package com.vvainer.eggtimer
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.vvainer.eggtimer.ui.theme.ComposeChallengeCardFlipTheme
+
+class EggTimer : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeChallengeCardFlipTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposeChallengeCardFlipTheme {
+        Greeting("Android")
+    }
+}
