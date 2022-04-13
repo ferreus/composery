4fa40d3fae8ca0d67ebe3651569433ca1fbd7961
Vladimir Vainer <str3l0k@gmail.com>
2022-04-13 20:11:56 +0300

    eggtimer: interactive

--- eggtimer/src/main/java/com/vvainer/eggtimer/EggTimer.kt
+++ eggtimer/src/main/java/com/vvainer/eggtimer/EggTimer.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.gestures.draggable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
@@ -15,7 +17,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Pause
 import androidx.compose.material.icons.filled.Refresh
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -27,6 +29,7 @@ import androidx.compose.ui.graphics.*
 import androidx.compose.ui.graphics.drawscope.*
 import androidx.compose.ui.graphics.rotate
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.Paragraph
@@ -35,9 +38,12 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.vvainer.eggtimer.ui.theme.ComposeChallengeCardFlipTheme
+import kotlin.math.abs
+import kotlin.math.atan2
 import kotlin.math.cos
 import kotlin.math.sin
 
@@ -63,20 +69,22 @@ class EggTimer : ComponentActivity() {
 
 @Composable
 fun EggTimerScreen() {
+    var currentTimeInSeconds by remember {
+        mutableStateOf(0)
+    }
     Column(modifier = Modifier.fillMaxHeight(), horizontalAlignment = Alignment.CenterHorizontally) {
-        TimerLabel()
+        TimerLabel(currentTimeInSeconds)
         Box(
             modifier = Modifier
-                .background(Color.Blue)
                 .fillMaxWidth()
                 .aspectRatio(1f)
                 .padding(30.dp)
         ) {
-            Box(
-                Modifier
-                    .background(Color.DarkGray)
-                    .fillMaxSize()
-            )
+            TimerDialerInteractive(0,
+                Modifier.fillMaxSize()
+            ) {
+                currentTimeInSeconds = it
+            }
         }
         Spacer(
             modifier = Modifier
@@ -121,9 +129,11 @@ fun EggTimerButton(
 }
 
 @Composable
-fun TimerLabel() {
+fun TimerLabel(currentTimeInSeconds: Int) {
+    val minutes = currentTimeInSeconds / 60
+    val seconds = currentTimeInSeconds - minutes*60
     Text(
-        "15:23",
+        "%02d:%02d".format(minutes,seconds),
         Modifier
             .padding(16.dp)
             .fillMaxWidth(),
@@ -142,7 +152,7 @@ fun TimerLabelPreview() {
         TimerLabel()
     }
 }
-
+*/
 
 @Preview(showBackground = true)
 @Composable
@@ -152,47 +162,51 @@ fun DefaultPreview() {
     }
 }
 
- */
+
 
 
 @Composable
-fun TimerDialer(value: Int) {
-    val textStyle = MaterialTheme.typography.h5
-    val arrowAngle = ((value.toFloat() / 3600f)*(Math.PI*2)).toDegrees()
+fun TimerDialer(currentTimeInSeconds: Int, modifier : Modifier = Modifier) {
+    val maxTimeInSeconds = 30f*60f
+    val pct = currentTimeInSeconds / maxTimeInSeconds
+    val arrowAngle = (pct*(Math.PI*2-Math.PI*2f/7f)).toDegrees()
     val paddingPx : Float = with(LocalDensity.current) { 65.dp.toPx() }
-    BoxWithConstraints(modifier = Modifier
+    BoxWithConstraints(modifier = modifier
         .fillMaxWidth()
         .padding(24.dp)
         .aspectRatio(1f)
         .shadow(8.dp, CircleShape, false)
         .background(Brush.verticalGradient(listOf(gradientTop, gradientBottom)), CircleShape)
         .drawBehind {
-            var radius = (size.width / 2f - paddingPx) + 45f
-            var angle = -Math.PI/2f
+            val radius = (size.width / 2f - paddingPx) + 45f
+            var angle = -Math.PI / 2f
             val ticks = 35
             val angleStep: Double = ((2 * Math.PI) / ticks)
             for (i in 0 until ticks) {
                 val r = if (i.rem(5) == 0) radius + 25f else radius
                 val sw = if (i.rem(5) == 0) 5f else 3f
-                val sx = center.x + ((radius-20f) * cos(angle)).toFloat()
-                val sy = center.y + ((radius-20f) * sin(angle)).toFloat()
+                val sx = center.x + ((radius - 20f) * cos(angle)).toFloat()
+                val sy = center.y + ((radius - 20f) * sin(angle)).toFloat()
                 val tx = center.x + (r * cos(angle)).toFloat()
                 val ty = center.y + (r * sin(angle)).toFloat()
                 angle += angleStep
-                drawLine(Color.DarkGray,Offset(sx,sy), Offset(tx, ty), strokeWidth = sw)
+                drawLine(Color.DarkGray, Offset(sx, sy), Offset(tx, ty), strokeWidth = sw)
             }
-            //radius+=25f
-            angle = -Math.PI/2f
+            angle = -Math.PI / 2f
             val tx = center.x + (radius * cos(angle)).toFloat()
             val ty = center.y + (radius * sin(angle)).toFloat()
             rotate(arrowAngle.toFloat()) {
                 val path = Path()
                 path.moveTo(tx, ty)
-                path.lineTo(tx-25f, ty+40f)
-                path.lineTo(tx+25f, ty+40f)
+                path.lineTo(tx - 25f, ty + 40f)
+                path.lineTo(tx + 25f, ty + 40f)
                 path.close()
                 drawPath(path, Color.Black, style = Fill)
-                drawOutline(Outline.Generic(path), Brush.horizontalGradient(listOf(Color.DarkGray, Color.LightGray)), style = Stroke(width = 1f))
+                drawOutline(
+                    Outline.Generic(path),
+                    Brush.horizontalGradient(listOf(Color.DarkGray, Color.LightGray)),
+                    style = Stroke(width = 1f)
+                )
             }
         }
     ) {
@@ -201,15 +215,17 @@ fun TimerDialer(value: Int) {
         for (i in 0..6) {
             Box(
                  modifier = Modifier
-                    .fillMaxSize()
-                    .align(Alignment.TopCenter)
-                    .padding(20.dp)
+                     .fillMaxSize()
+                     .align(Alignment.TopCenter)
+                     .padding(20.dp)
 
-                    .rotate(i*(360f/7f))) {
+                     .rotate(i * (360f / 7f))) {
 
                 Text(
                     "${i * 5}",
-                    modifier = Modifier.fillMaxWidth().rotate(angles[i]),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .rotate(angles[i]),
                     textAlign = TextAlign.Center
                 )
             }
@@ -229,13 +245,52 @@ fun TimerDialer(value: Int) {
                 Image(painter = painterResource(id = R.drawable.kotlin),
                     colorFilter = ColorFilter.tint(Color.Black),
                     contentDescription = "kotlin logo", modifier = Modifier
-                    .size(48.dp)
-                    .align(
-                        Alignment.Center
-                    ))
+                        .size(48.dp)
+                        .align(
+                            Alignment.Center
+                        )
+                        .rotate(arrowAngle))
             }
         }
     }
+}
+
+
+@Composable
+fun TimerDialerInteractive(currentTimeInSeconds: Int, modifier : Modifier = Modifier, onTimeChanged: (Int) -> Unit = {}) {
+    var currentTime by remember { mutableStateOf(currentTimeInSeconds)}
+    var dragPoint = Offset(0f, 0f)
+    TimerDialer(currentTimeInSeconds = currentTime,
+        modifier = modifier.pointerInput(Unit) {
+            detectDragGestures(onDragStart = {
+                dragPoint = it
+            }, onDragEnd = {
+
+            }) { change, dragAmount ->
+                dragPoint += dragAmount
+                currentTime = getCurrentTime(dragPoint, size)
+                Log.e("DRAG","X:${dragPoint.x}, Y:${dragPoint.y} size:${size.width}, ${size.height}")
+                onTimeChanged(currentTime)
+            }
+        })
+}
+
+fun getCurrentTime(dragCenter: Offset, size: IntSize): Int {
+    val radius = size.width/2f
+    val pos  = Offset(dragCenter.x - radius, dragCenter.y - radius)
+    val (dx,dy) = Offset(0f,0f) - pos
+    var theta = atan2(dy,dx)
+    while (theta<0f) {
+        theta+= Math.PI.toFloat()*2f
+    }
+    while (theta>Math.PI*2) {
+        theta -= Math.PI.toFloat()*2f
+    }
+    theta -= Math.PI.toFloat()/2f
+    val pct = theta / (Math.PI*2f)
+    val seconds = pct * (45*60)
+    Log.e("DRAG","pos:${pos.x},${pos.y} -> dx,dy:$dx,$dy => theta:$theta, pct:$pct, seconds:$seconds")
+    return seconds.toInt().coerceIn(0,35*60)
 }
 
 private fun Double.toDegrees(): Float {
@@ -246,6 +301,6 @@ private fun Double.toDegrees(): Float {
 @Composable
 fun TimerDialerPreview() {
     ComposeChallengeCardFlipTheme {
-        TimerDialer(15*60)
+        TimerDialer(5)
     }
 }
