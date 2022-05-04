51b257aee2402c9956bccee3859bd4ea9131875a
Vladimir Vainer <str3l0k@gmail.com>
2022-05-04 22:11:19 +0300

    featurediscovery: ui test bed

--- featurediscovery/src/main/java/com/vvainer/featurediscovery/FeatureDiscovery.kt
+++ featurediscovery/src/main/java/com/vvainer/featurediscovery/FeatureDiscovery.kt
@@ -4,10 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
@@ -15,9 +12,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.vvainer.featurediscovery.ui.theme.ComposeryTheme
 
 class FeatureDiscovery : ComponentActivity() {
@@ -61,8 +62,63 @@ fun Main() {
             contentColor = Color.White
         )},
         content = {
-
+            Body()
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = { /*TODO*/ }) {
+                Icon(Icons.Default.Add, contentDescription = "")
+            }
         }
     )
+}
+
+@Composable
+fun Body() {
+    Column {
+        AsyncImage(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data("https://www.caffeineinformer.com/wp-content/uploads/starbucks-image.jpg")
+                .crossfade(true).build(),
+            contentDescription = "",
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp),
+            contentScale = ContentScale.FillWidth
+        )
+        Box(modifier = Modifier.fillMaxWidth()) {
+            Column(
+                Modifier
+                    .fillMaxWidth()
+                    .background(MaterialTheme.colors.primary)
+                    .padding(16.dp)
+            ) {
+                Text(
+                    "Starbucks Coffee",
+                    fontSize = 24.sp,
+                    color = Color.White,
+                    modifier = Modifier.padding(bottom = 8.dp)
+                )
+                Text("Coffee Shop", fontSize = 16.sp, color = Color.White)
+            }
+            FloatingActionButton(
+                onClick = { /*TODO*/ },
+                modifier = Modifier.align(alignment = Alignment.CenterEnd).offset(
+                    x = (-16).dp,
+                    y = (-48).dp
+                ),
+                backgroundColor = Color.White,
+                contentColor = MaterialTheme.colors.primary
+            ) {
+                Icon(Icons.Default.DriveEta, contentDescription = "")
+            }
+        }
+        OutlinedButton(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            onClick = { /*TODO*/ }) {
+            Text("Do Feature Discovery")
+        }
+    }
 }
 
--- featurediscovery/src/main/java/com/vvainer/featurediscovery/ui/theme/Theme.kt
+++ featurediscovery/src/main/java/com/vvainer/featurediscovery/ui/theme/Theme.kt
@@ -5,17 +5,18 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = Color(0xff0da2ff),
+    primaryVariant = Color.Blue.copy(blue = 0.5f),
+    secondary = Color.Blue
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = Color(0xff0da2ff),
+    primaryVariant = Color(0xff0da2ff),
+    secondary = Color(0xff0da2ff),
 
     /* Other default colors to override
     background = Color.White,
